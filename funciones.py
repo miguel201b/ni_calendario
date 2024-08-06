b0c2da3ac0163c2a4d7f98d39a8ec49bc9573063
miguel201b <miguelcniyc@gmail.com>
2024-08-06 07:59:16 -0600

    quitar bvariable de entorno de print

--- funciones.py
+++ funciones.py
@@ -80,7 +80,6 @@ def obtener_horas(inicial, turno):
 
 
 def insertarEstadisticas(letra,apellido):
-    print("Variables de entorno disponibles:", os.environ)
     # Obtener la URL de la base de datos desde la variable de entorno
     database_url = os.getenv('apikeyEstadisticas')
     if database_url is None:
